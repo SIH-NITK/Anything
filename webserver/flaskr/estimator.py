aec8e92fc82d69785fe8d197b024b3f86b6afb11
Abhishek Kumar <abhishekkumar8222@gmail.com>
2020-01-22 11:45:05 +0530

    Webserver: Integrate ML alogrithm in web app flow

--- webserver/flaskr/estimator.py
+++ webserver/flaskr/estimator.py
@@ -8,6 +8,11 @@ from flask.cli import with_appcontext
 
 from flaskr.db import get_db, close_db
 
+import cv2
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
 def calculate(dataset_id):
     """Calculates all values associated with a dataset."""
 
@@ -19,6 +24,27 @@ def calculate(dataset_id):
     # Create temporary directory
     with tempfile.TemporaryDirectory(dir='uploads') as temp_dir:
         root_path = extract_zip(dataset_filepath, temp_dir)
+
+        nvdi_density = []
+        image_files = sorted(os.listdir(root_path))
+
+        for image_file in image_files:
+            # Read image in grey scale
+            image = cv2.imread(os.path.join(root_path, image_file), cv2.IMREAD_GRAYSCALE)
+
+            # Select pixels which greater than threshold
+            _, image = cv2.threshold(image, np.mean(image), 255, cv2.THRESH_BINARY)
+
+            # Invert image
+            image = cv2.bitwise_not(image)
+
+            # Calculate number of crop pixels per pixel
+            nvdi_density.append(cv2.countNonZero(image)/(image.shape[0] * image.shape[1]))
+            # nvdi_density.append(cv2.countNonZero(image)/(image.shape[0] * image.shape[1]))
+
+        plt.plot(range(len(image_files)), nvdi_density, 'ro-')
+        plt.savefig(os.path.join('flaskr', 'static', 'results', str(dataset_id) + '.png'))
+
 
     # There should be a 2D or 2 1D array that represent x and y values
 
